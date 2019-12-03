1288afe6ff1eaf3ea19e4e7118b13ac71d0669d2
hspaula <hudsonsistemasp@gmail.com>
2019-12-02 23:59:49 -0300

    Criação entity e resource da classe User

---
+++ src/main/java/com/example/courseJPA/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.courseJPA.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.courseJPA.entities.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+	//O tipo de retorno ResponseEntity, é um tipo específico do spring para retornar respostas de requisições web
+	@GetMapping //Tipo GET do HTTP
+	public ResponseEntity<User> findAll(){
+		User user = new User(1L,"Hudson","email@email.com","123");
+		return ResponseEntity.ok().body(user);
+	}
+}
